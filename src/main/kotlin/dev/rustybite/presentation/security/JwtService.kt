7912b4fb195cd1511425ef60c62ede38258057ac
Atanas Charle <atanasi.dev@gmail.com>
2024-06-11 23:42:15 +0300

    create JwtService to generate JWT, verify and validate

---
+++ src/main/kotlin/dev/rustybite/presentation/security/JwtService.kt
@@ -0,0 +1,39 @@
+package dev.rustybite.presentation.security
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import dev.rustybite.domain.models.User
+import dev.rustybite.domain.models.Users
+import io.ktor.server.auth.jwt.*
+import org.jetbrains.exposed.sql.Database
+import java.util.*
+
+class JwtService {
+    private val secret = System.getenv("JWT_SECRET")
+    private val issuer = System.getenv("ISSUER")
+    private val audience = System.getenv("AUDIENCE")
+    private val realm = System.getenv("REALM")
+    private val algorithm = Algorithm.HMAC256(secret)
+
+    val verifier = JWT.require(algorithm)
+        .withIssuer(issuer)
+        .withAudience(audience)
+        .build()
+
+    fun generateToken(user: User): String {
+        return JWT.create()
+            .withAudience(audience)
+            .withIssuer(issuer)
+            .withClaim("email", user.email)
+            .withExpiresAt(Date(System.currentTimeMillis() + 600000))
+            .sign(algorithm)
+    }
+
+    fun validate(credential: JWTCredential): JWTPrincipal? {
+        if (credential.payload.getClaim("email").asString() != "") {
+            JWTPrincipal(credential.payload)
+        } else {
+            null
+        }
+    }
+}
