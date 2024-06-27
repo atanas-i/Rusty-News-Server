4eb1247f61d13c5902ad9ad8448eae3e6aac4abb
Atanas Charle <atanasi.dev@gmail.com>
2024-06-27 21:48:34 +0300

    fix login as well adding/fixing migration files

--- src/main/kotlin/dev/rustybite/presentation/routes/UserRoute.kt
+++ src/main/kotlin/dev/rustybite/presentation/routes/UserRoute.kt
@@ -13,6 +13,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.isActive
 
 fun Route.userRoute(
     userRepository: UserRepository,
